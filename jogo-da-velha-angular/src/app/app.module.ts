89fcf2598d8b687d0a65288cd7a182451662655d
condor <e.hhdelemos@gmail.com>
2020-07-07 20:05:15 -0300

    Cria modulo jogo da velha

--- jogo-da-velha-angular/src/app/app.module.ts
+++ jogo-da-velha-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
